a0879b89072fb3c703e2689308961aff41b102ed
Jiaying Song <90585055+jessicasong78@users.noreply.github.com>
2021-09-18 19:35:03 -0700

    Add files via upload

---
+++ People.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test2
+{
+    class People
+    {
+        private List<Person> _list = new List<Person>();
+
+        public int Count { get; }
+
+        public void Add(Person person)
+        {
+            _list.Add(person);
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public IEnumerable<Person> GetAllPeople()
+        {
+            return _list;
+        }
+        //This method filted people whose mobile type is Android
+        public IEnumerable<Person> GetAndroidPeople()
+        {
+            var android = new List<Person>();
+
+            foreach (var person in _list)
+            {
+                if (person.MobileType == MobileType.Android)
+                {
+                    android.Add(person);
+                }
+            }
+            return android;
+        }
+
+        //This method filted people whose mobile type is Apple
+        public IEnumerable<Person> GetApplePeople()
+        {
+            var apple = new List<Person>();
+
+            foreach (var person in _list)
+            {
+                if (person.MobileType == MobileType.Apple)
+                {
+                    apple.Add(person);
+                }
+            }
+            return apple;
+        }
+
+    }
+}
